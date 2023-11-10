ec521e4c11c793db3c93a5ef9a679d5339da3792
Jilll3542 <xu20213542@163.com>
2023-11-10 15:51:16 +0800

    搭建VUEPRESS

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,24 @@
+import { defaultTheme, defineUserConfig } from 'vuepress'
+
+
+export default defineUserConfig({
+  lang: 'zh-CN',
+  title: '你好， VuePress ！',
+  description: '这是我的第一个 VuePress 站点',
+  base: '/vuepress-theme-hope/',
+  head: [
+    
+  ],
+  theme:defaultTheme({
+
+    navbar:[
+      {
+        text:"jilll空间",
+        link:"https://github.com/Jilll3542/Jilll3542.github.io"
+      },
+    ],
+    repo:"Jilll3542/vuepress-theme-hope",
+    sidebar:[]
+  })
+  
+})
