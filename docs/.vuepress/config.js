7d4b242508f81a2baf42b2ed3783c9809d2120d9
Jilll3542 <xu20213542@163.com>
2023-11-10 16:03:04 +0800

    修改base配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,7 +5,7 @@ export default defineUserConfig({
   lang: 'zh-CN',
   title: '你好， VuePress ！',
   description: '这是我的第一个 VuePress 站点',
-  base: '/vuepress-theme-hope/',
+  base: '/',
   head: [
     
   ],
